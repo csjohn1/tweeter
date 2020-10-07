65b6e32b7c306fc13c6b52066bb93108cc9b7968
csjohn1 <csjohn@shaw.ca>
2020-10-07 17:03:32 +0000

    will change character counter font if count goes below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   $('#tweet-text').on('input', function () {
     let counter = 140 - $(this).val().length;
+    if (counter < 0) {
+      document.getElementById("counter").style.color = "red";
+    } else {
+      document.getElementById("counter").style.color = "#606060";
+    }
     $(this).closest('form').find('#counter').text(counter);
   })  
 });
