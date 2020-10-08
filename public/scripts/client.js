5cacf99487256678d742f39ffd6c0f24c3d03500
csjohn1 <csjohn@shaw.ca>
2020-10-08 22:01:58 +0000

    ensured form input is valid and XSS secure. created drop-down error warnings upon invalid submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,84 +6,79 @@
 
 $(document).ready(function () {
 
-  // const data = [
-  //   {
-  //     "user": {
-  //       "name": "Newton",
-  //       "avatars": "https://i.imgur.com/73hZDYK.png"
-  //       ,
-  //       "handle": "@SirIsaac"
-  //     },
-  //     "content": {
-  //       "text": "If I have seen further it is by standing on the shoulders of giants"
-  //     },
-  //     "created_at": 1461116232227
-  //   },
-  //   {
-  //     "user": {
-  //       "name": "Descartes",
-  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //       "handle": "@rd" },
-  //     "content": {
-  //       "text": "Je pense , donc je suis"
-  //     },
-  //     "created_at": 1461113959088
-  //   }
-  // ]
-
-  const renderTweets = function(tweets) {
+  // $('form').on('submit', function() {
+  //   console.log("hello");
+  //   $(".error").slideDown();
+  // });
+  
+  const renderTweets = function (tweets) {
     for (let tweet of tweets) {
       let $tweet = createTweetElement(tweet);
       $('#tweets-container').append($tweet);
     }
   }
+  const createTweetElement = function (tweet) {
+    const escape =  function(str) {
+      let div = document.createElement('div');
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML;
+    }
+    let $tweet = "";
+    $tweet += `<article class="tweetcontainer">`
+    $tweet += "<header>"
+    $tweet += `<p class="header-right">`
+    $tweet += `<img src=${tweet.user.avatars}>`
+    $tweet += `${tweet.user.name}</p>`
+    $tweet += `<p class="handle">${tweet.user.handle}</p>`
+    $tweet += "</header>"
+    $tweet += `<div class="tweet-body">`
+    $tweet += `<p>${escape(tweet.content.text)}</p>`
+    $tweet += "</div>"
+    $tweet += "<footer>"
+    $tweet += `<p class="feet">${tweet.created_at}</p>`
+    //  $tweet += `<p class="feet">${Date.now()}</p>`
+    $tweet += `<p class="feet">misc</p>`
+    $tweet += "</footer>"
+    $tweet += "</article>"
+    return $tweet;
+  }
 
-const createTweetElement = function(tweet) {
-  let $tweet = "";
-  $tweet += `<article class="tweetcontainer">`
-   $tweet += "<header>"
-   $tweet += `<p class="header-right">`
-   $tweet += `<img src=${tweet.user.avatars}>`
-   $tweet += `${tweet.user.name}</p>`
-   $tweet += `<p class="handle">${tweet.user.handle}</p>`
-   $tweet += "</header>"
-   $tweet += `<div class="tweet-body">`
-   $tweet += `<p>${tweet.content.text}</p>`
-   $tweet += "</div>"
-   $tweet += "<footer>"
-   $tweet += `<p class="feet">${tweet.created_at}</p>`
-  //  $tweet += `<p class="feet">${Date.now()}</p>`
-   $tweet += `<p class="feet">misc</p>`
-   $tweet += "</footer>"
-   $tweet += "</article>"
-  return $tweet;
-}
-
-  $('form').on('submit', function () {
+  $('form').on('submit', function (event) {
     event.preventDefault()
-    console.log('Button clicked, performing ajax call...');
-    $.ajax({
-      url:'/tweets/', 
-      method: 'POST',
-      data: $('#tweet-text').serialize()
-    })
-    .then(function () {
-      console.log('Success');
-      console.log($('#tweet-text').serialize());
-      console.log($(this).closest('form').find('#tweet-text').text('#tweet-text').serialize());
-    });
+    if ($('#tweet-text').val().length > 140) {
+      $(".error1").append('twete 2 long');
+      $(".error").slideDown();
+    } else if ($('#tweet-text').val() === '') {
+      $(".error2").append('where the twete');
+      $(".error").slideDown();
+    } else {
+      $(".error").slideUp();
+      $(".error1").empty();
+      $(".error2").empty();
+      console.log('Button clicked, performing ajax call...');
+      $.ajax({
+        url: '/tweets/',
+        method: 'POST',
+        data: $('form').serialize()
+      })
+        .then(function () {
+          loadTweets();
+          console.log('Success');
+        });
+    }
   });
 
-const loadTweets = function() {
+  const loadTweets = function () {
     console.log("YES!")
     $.ajax({
       url: "/tweets/",
       method: "GET"
     })
-    .then(function(data) {
-      renderTweets(data);
-    })
-}
-loadTweets();
-
+      .then(function (data) {
+        let reverseData = data.reverse();
+        $("#tweets-container").empty();
+        renderTweets(reverseData);
+      })
+  }
+  loadTweets();
 });
