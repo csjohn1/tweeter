50fef59d56fce53f2cfa3ba713fda2f50f6cc481
csjohn1 <csjohn@shaw.ca>
2020-10-09 17:01:31 +0000

    final clean-up

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,47 +4,42 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {
+$(document).ready(function() {
 
-  // $('form').on('submit', function() {
-  //   console.log("hello");
-  //   $(".error").slideDown();
-  // });
-  
-  const renderTweets = function (tweets) {
+  const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       let $tweet = createTweetElement(tweet);
       $('#tweets-container').append($tweet);
     }
-  }
-  const createTweetElement = function (tweet) {
+  };
+  const createTweetElement = function(tweet) {
     const escape =  function(str) {
       let div = document.createElement('div');
       div.appendChild(document.createTextNode(str));
       return div.innerHTML;
-    }
+    };
     let $tweet = "";
-    $tweet += `<article class="tweetcontainer">`
-    $tweet += "<header>"
-    $tweet += `<p class="header-right">`
-    $tweet += `<img src=${tweet.user.avatars}>`
-    $tweet += `${tweet.user.name}</p>`
-    $tweet += `<p class="handle">${tweet.user.handle}</p>`
-    $tweet += "</header>"
-    $tweet += `<div class="tweet-body">`
-    $tweet += `<p class="tweet-text">${escape(tweet.content.text)}</p>`
-    $tweet += "</div>"
-    $tweet += "<footer>"
-    $tweet += `<p class="feet">${tweet.created_at}</p>`
+    $tweet += `<article class="tweetcontainer">`;
+    $tweet += "<header>";
+    $tweet += `<p class="header-right">`;
+    $tweet += `<img src=${tweet.user.avatars}>`;
+    $tweet += `${tweet.user.name}</p>`;
+    $tweet += `<p class="handle">${tweet.user.handle}</p>`;
+    $tweet += "</header>";
+    $tweet += `<div class="tweet-body">`;
+    $tweet += `<p class="tweet-text">${escape(tweet.content.text)}</p>`;
+    $tweet += "</div>";
+    $tweet += "<footer>";
+    $tweet += `<p class="feet">${tweet.created_at}</p>`;
     //  $tweet += `<p class="feet">${Date.now()}</p>`
-    $tweet += `<p class="feet">Retweet</p>`
-    $tweet += "</footer>"
-    $tweet += "</article>"
+    $tweet += `<p class="feet" id="flag">Flag Retweet Like</p>`;
+    $tweet += "</footer>";
+    $tweet += "</article>";
     return $tweet;
-  }
+  };
 
-  $('form').on('submit', function (event) {
-    event.preventDefault()
+  $('form').on('submit', function(event) {
+    event.preventDefault();
     if ($('#tweet-text').val().length > 140) {
       $(".error1").append('twete 2 long');
       $(".error").slideDown();
@@ -61,24 +56,24 @@ $(document).ready(function () {
         method: 'POST',
         data: $('form').serialize()
       })
-        .then(function () {
+        .then(function() {
           loadTweets();
           console.log('Success');
         });
     }
   });
 
-  const loadTweets = function () {
-    console.log("YES!")
+  const loadTweets = function() {
+    console.log("YES!");
     $.ajax({
       url: "/tweets/",
       method: "GET"
     })
-      .then(function (data) {
+      .then(function(data) {
         let reverseData = data.reverse();
         $("#tweets-container").empty();
         renderTweets(reverseData);
-      })
-  }
+      });
+  };
   loadTweets();
 });
